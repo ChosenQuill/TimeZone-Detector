e14da15a3e2a33886064f54d4269fbc3ba638a2e
SureDroid <support@suredroid.com>
2019-07-29 10:33:08 -0500

    Added CDT option plus more info

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
+
 let utc = -(new Date().getTimezoneOffset() / 60);
 let name = Intl.DateTimeFormat().resolvedOptions().timeZone;
 let short = new Date().toLocaleTimeString('en-us',{timeZoneName:'short'}).split(' ')[2];
 
-
 document.getElementById("timezone").textContent = name;
 document.getElementById("short").textContent = short;
-document.getElementById("utc").textContent = (utc<0?"":"+") + utc.toString();
+if(window.location.hash.substr(1).toLowerCase() === "cst") {
+    let cst = utc + 6;
+    document.getElementById("bottom").textContent += "CST" + (cst < 0 ? "" : "+") + cst.toString() + " (" + Math.abs(cst) + " Hour" + (Math.abs(cst) === 1 ? "" : "s") + " " + (cst > 0 ? "Ahead" : "Behind") + ")";
+} else {
+    document.getElementById("bottom").textContent = "UTC" + (utc < 0 ? "" : "+") + utc.toString() + " (" + Math.abs(utc) + " Hour" + (Math.abs(utc) === 1 ? "" : "s")+ " " + (utc > 0 ? "Ahead" : "Behind") + ")";
+}
